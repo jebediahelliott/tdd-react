8f58c9ccab60c264982bea9c7face17eccc45887
jebediahelliott <jebediahelliott@gmail.com>
2019-03-19 12:42:32 -0400

    Add test to check correct number of comments rendered.

--- src/containers/CommentFeed.test.js
+++ src/containers/CommentFeed.test.js
@@ -17,4 +17,22 @@ describe('CommentFeed', () => {
     const commentNodes = container.querySelectorAll('.Comment')
     expect(commentNodes.length).toBe(props.comments.length)
   })
+
+  it('renders the comment list with some entries', () => {
+    let comments = [
+      {
+        author: 'Ian Wilson',
+        text: 'A boats a boat but a mystery box could be anything.'
+      },
+      {
+        author: 'Max Powers Jr',
+        text: 'Krypton sucks.',
+      }
+    ]
+
+    props.comments = comments 
+    const {container} = render(<CommentFeed {...props} />)
+    const commentNodes = container.querySelectorAll('.Comment')
+    expect(commentNodes.length).toBe(props.comments.length)
+  })
 })
